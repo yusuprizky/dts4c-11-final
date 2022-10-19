f474bf75fc9bfc13ae27340021735971ce0d7c5a
Yusup Rizky Rinaldy <yusuprizkyr29@gmail.com>
2022-10-19 19:26:22 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import HotTopicsPage from './pages/HotTopicsPage';
+import LatestNewsPage from './pages/LatestNewsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="hottopics/:id" element={<HotTopicsPage />} />
+        <Route path="latestnews/:id" element={<LatestNewsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
