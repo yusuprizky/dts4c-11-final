89cccc329b34801d803b7bd8a040be191ce0cc08
Vananda Rahadika <vanandarahadika79@gmail.com>
2022-10-21 21:50:25 +0700

    Update : Tampilan footer stick di bawah

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,61 @@
-import { AppBar, Toolbar, Typography, Grid } from "@mui/material/";
+import { AppBar, Paper, Toolbar, Typography, Grid } from "@mui/material/";
 import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer style={{ marginTop: "5rem", backgroundColor: "#121212" }}>
-      <AppBar position="static" elevation={0} component="footer" color="default">
-        <Toolbar style={{ justifyContent: "center" }}>
-          <Typography variant="p" noWrap component="div" sx={{ flexGrow: 1, alignSelf: "center" }}>
-            <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
-              REA4C Berita
-            </Link>
+    <Paper
+      sx={{
+        marginTop: "calc(10% + 60px)",
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+      }}
+      component="footer"
+      square
+    >
+      <Toolbar style={{ justifyContent: "center" }}>
+        <Typography
+          variant="p"
+          noWrap
+          component="div"
+          sx={{ flexGrow: 1, alignSelf: "center" }}
+        >
+          <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
+            REA4C Berita
+          </Link>
+        </Typography>
+        <Typography
+          variant="p"
+          noWrap
+          component="div"
+          sx={{
+            flexGrow: 1,
+            alignSelf: "center",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="caption">©2022</Typography>
+        </Typography>
+        <Typography
+          variant="p"
+          noWrap
+          component="div"
+          sx={{
+            flexGrow: 1,
+            alignSelf: "center",
+            display: "flex",
+            justifyContent: "end",
+          }}
+        >
+          <Typography variant="caption">
+            <Facebook sx={{ marginRight: "5px", cursor: "pointer" }} />
+            <Instagram sx={{ marginRight: "5px", cursor: "pointer" }} />
+            <Twitter sx={{ marginRight: "5px", cursor: "pointer" }} />
           </Typography>
-          <Typography
-            variant="p"
-            noWrap
-            component="div"
-            sx={{
-              flexGrow: 1,
-              alignSelf: "center",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Typography variant="caption">©2022</Typography>
-          </Typography>
-          <Typography
-            variant="p"
-            noWrap
-            component="div"
-            sx={{
-              flexGrow: 1,
-              alignSelf: "center",
-              display: "flex",
-              justifyContent: "end",
-            }}
-          >
-            <Typography variant="caption">
-              <Facebook sx={{ marginRight: "5px", cursor: "pointer" }} />
-              <Instagram sx={{ marginRight: "5px", cursor: "pointer" }} />
-              <Twitter sx={{ marginRight: "5px", cursor: "pointer" }} />
-            </Typography>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </footer>
+        </Typography>
+      </Toolbar>
+    </Paper>
   );
 }
