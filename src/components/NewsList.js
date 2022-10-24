2725040666ba5b11354b40b21959bb19a65f8265
Vananda Rahadika <vanandarahadika79@gmail.com>
2022-10-24 20:09:52 +0700

    Update : Tambah Spinner ketika loading

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,45 +1,71 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getNews } from "../features/newsSlice";
+import Spinner from "react-spinner-material";
 
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
 const NewsList = () => {
   const dispatch = useDispatch();
-  const { entities, category } = useSelector((state) => state.news);
+  const { entities, category, loading } = useSelector((state) => state.news);
   // console.log(entities);
 
   useEffect(() => {
     dispatch(getNews(""));
   }, [dispatch]);
 
-  return (
-    <>
-      <Typography variant="h4" sx={{ fontFamily: "Roboto", marginBottom: "1rem" }}>
-        {category}
-      </Typography>
-      {entities.articles?.map((data, i) => {
-        return (
-          <Card key={i} sx={{ marginBottom: 5, boxShadow: "2px 2px 4px 4px rgba(0,0,0,0.4)" }}>
-            <CardMedia component="img" height="200" image={data.urlToImage} sx={{ objectFit: "none" }} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {data.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {data.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button href={data.url} target="_blank" size="small">
-                Read More
-              </Button>
-            </CardActions>
-          </Card>
-        );
-      })}
-    </>
-  );
+  if (loading) {
+    return <Spinner radius={120} color={"#333"} stroke={2} visible={true} />;
+  } else {
+    return (
+      <>
+        <Typography
+          variant="h4"
+          sx={{ fontFamily: "Roboto", marginBottom: "1rem" }}
+        >
+          {category}
+        </Typography>
+        {entities.articles?.map((data, i) => {
+          return (
+            <Card
+              key={i}
+              sx={{
+                marginBottom: 5,
+                boxShadow: "2px 2px 4px 4px rgba(0,0,0,0.4)",
+              }}
+            >
+              <CardMedia
+                component="img"
+                height="200"
+                image={data.urlToImage}
+                sx={{ objectFit: "none" }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {data.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button href={data.url} target="_blank" size="small">
+                  Read More
+                </Button>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </>
+    );
+  }
 };
 
 export default NewsList;
