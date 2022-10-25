820deddd9c1cf833494e6d39f794b568dda3e248
Vananda Rahadika <vanandarahadika79@gmail.com>
2022-10-25 21:07:07 +0700

    Update : Pagination ke tengah, page setelah loading adalah currentPage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,9 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const dispatch = useDispatch();
-  let { totalData, loading, messageError } = useSelector((state) => state.news);
+  let { totalData, loading, messageError, currentPage } = useSelector(
+    (state) => state.news
+  );
   let totalPage = Math.ceil(totalData / 10);
   const [page, setPage] = useState(1);
 
@@ -26,37 +28,40 @@ const Home = () => {
   }, [dispatch, page]);
 
   const handlePage = (event, value) => {
+    console.log(value);
     setPage(value);
   };
 
   return (
-    <Container sx={{ height: "100%" }}>
-      <Grid container spacing={2} sx={{ marginTop: "2rem" }}>
-        <Grid item xs={12} md={12}>
-          {(() => {
-            if (loading === "loading") {
-              return (
-                <Stack alignItems="center" height="100vh">
-                  <CircularProgress color="primary" />
-                </Stack>
-              );
-            } else if (loading === "failed") {
-              return (
-                <Stack alignItems="center" height="100vh">
-                  <Typography variant="h5">{messageError}</Typography>
-                </Stack>
-              );
-            } else {
-              return (
-                <div>
-                  <NewsList />
-                  <Pagination count={totalPage} onChange={handlePage} />
-                </div>
-              );
-            }
-          })()}
-        </Grid>
-      </Grid>
+    <Container sx={{ height: "100%", marginTop: "2rem" }}>
+      {(() => {
+        if (loading === "loading") {
+          return (
+            <Stack alignItems="center" height="100vh">
+              <CircularProgress color="primary" />
+            </Stack>
+          );
+        } else if (loading === "failed") {
+          return (
+            <Stack alignItems="center" height="100vh">
+              <Typography variant="h5">{messageError}</Typography>
+            </Stack>
+          );
+        } else {
+          return (
+            <div>
+              <NewsList />
+              <Stack alignItems="center">
+                <Pagination
+                  page={currentPage}
+                  count={totalPage}
+                  onChange={handlePage}
+                />
+              </Stack>
+            </div>
+          );
+        }
+      })()}
     </Container>
   );
 };
